d7a190bae79d78a7b188be526f13201a74029365
Vinodha R <162858417+VinodhaRavi@users.noreply.github.com>
2025-06-27 14:44:44 +0530

    Delete exception directory

--- exception/order_not_found_exception.py
+++
@@ -1,3 +0,0 @@
-class OrderNotFoundException(Exception):
-    def __init__(self, message="Order not found"):
-        super().__init__(message)
--- exception/user_not_found_exception.py
+++
@@ -1,3 +0,0 @@
-class UserNotFoundException(Exception):
-    def __init__(self, message="User not found"):
-        super().__init__(message)
